6b7233d61af09d17f4541b2575ec279387541ea0
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-10-07 16:11:10 -0400

    move physics logic from main.py into physics.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import pygame
 import numpy
 import pymunk
 import pymunk.pygame_util
-from typing import Any
 
-from config import SCREEN_WIDTH, SCREEN_HEIGHT, LEFT, RIGHT, TOP, BOTTOM, MAX_FRUIT_TO_SPAWN
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, LEFT, RIGHT, MAX_FRUIT_TO_SPAWN
 from fruits import Fruit, draw_fruit, create_fruit, create_random_fruit
+from physics import create_static_boundaries, handle_fruit_collision
 
 
 pygame.init()
@@ -24,50 +24,9 @@ steps_per_frame = 1
 space = pymunk.Space()
 space.gravity = (0.0, 900.0)
 
-
-def create_static_boundaries() -> None:
-    """Initializes the game boundary with static line segments"""
-
-    static_body = space.static_body
-    static_lines = [
-        pymunk.Segment(static_body, (LEFT, TOP), (LEFT, BOTTOM), 5.0),
-        pymunk.Segment(static_body, (RIGHT, TOP), (RIGHT, BOTTOM), 5.0),
-        pymunk.Segment(static_body, (LEFT, TOP), (RIGHT, TOP), 5.0),
-    ]
-    for line in static_lines:
-        line.elasticity = 0.95
-        line.friction = 0.9
-    space.add(*static_lines)
-
-
-def handle_fruit_collision(arbiter: pymunk.Arbiter, space: pymunk.Space, data: dict[Any, Any]) -> bool:
-    """Handles fruit collisions
-    
-    Args:
-        arbiter: Information about the two collided shapes.
-        space: The space the collision occured in.
-        data: Additional information required for handling the collision.
-    
-    Returns:
-        True, as the collision has been processed.
-    """
-    shape1, shape2 = arbiter.shapes
-    fruit1, fruit2 = Fruit.get_fruits_from_shape(shape1, shape2)
-
-    if fruit1.id == fruit2.id:
-        next_fruit_id = fruit1.id + 1
-        for fruit in Fruit:
-            if fruit.id == next_fruit_id:
-                # Delete original fruits
-                space.remove(shape1, shape1.body)
-                space.remove(shape2, shape2.body)
-
-                # Spawn new fruit at the contact point of the two collided fruits
-                contact_point = arbiter.contact_point_set.points[0].point_a
-                new_x, new_y = contact_point[0], contact_point[1]
-                create_fruit(space, fruit, (new_x, new_y))
-
-    return True  # Collision should be processed
+# Collision configuration
+handler = space.add_collision_handler(1, 1)
+handler.pre_solve = handle_fruit_collision
 
 
 def render_pymunk_space(space: pymunk.Space) -> None:
@@ -91,12 +50,7 @@ def render_pymunk_space(space: pymunk.Space) -> None:
             pygame.draw.line(screen, "white", shape.a, shape.b, 5)
 
 
-# Collision configuration
-handler = space.add_collision_handler(1, 1)
-handler.pre_solve = handle_fruit_collision
-
-create_static_boundaries()
-
+create_static_boundaries(space)
 current_fruit = create_random_fruit(MAX_FRUIT_TO_SPAWN)
 on_cooldown = False
 cooldown_timer = 0
