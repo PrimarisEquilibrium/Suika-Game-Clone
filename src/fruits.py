1ff16f3718929606b54a31a430fa9af4b0f4b0b5
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-10-07 16:03:55 -0400

    add score to Fruit

--- src/fruits.py
+++ src/fruits.py
@@ -14,25 +14,27 @@ class Fruit(Enum):
         radius: Radius of the fruit.
         mass: Mass of the fruit.
         color: RGB color of the fruit.
+        score: Points earned when merged into.
     """
 
-    CHERRY     = (0, 12, 1.0, (239, 7, 10))
-    STRAWBERRY = (1, 16, 1.2, (251, 108, 75))
-    GRAPE      = (2, 24, 1.4, (166, 108, 252))
-    DEKOPON    = (3, 30, 2.0, (255, 185, 2))
-    ORANGE     = (4, 36, 2.5, (254, 140, 32))
-    APPLE      = (5, 44, 3.0, (244, 22, 22))
-    PEAR       = (6, 48, 3.2, (255, 249, 103))
-    PEACH      = (7, 56, 3.5, (254, 203, 197))
-    PINEAPPLE  = (8, 62, 4.5, (242, 243, 23))
-    MELON      = (9, 70, 5.0, (162, 239, 30))
-    WATERMELON = (10, 80, 6.0, (33, 126, 22))
+    CHERRY     = (0, 12, 1.0, (239, 7, 10), 1)
+    STRAWBERRY = (1, 16, 1.2, (251, 108, 75), 3)
+    GRAPE      = (2, 24, 1.4, (166, 108, 252), 6)
+    DEKOPON    = (3, 30, 2.0, (255, 185, 2), 10)
+    ORANGE     = (4, 36, 2.5, (254, 140, 32), 15)
+    APPLE      = (5, 44, 3.0, (244, 22, 22), 21)
+    PEAR       = (6, 48, 3.2, (255, 249, 103), 28)
+    PEACH      = (7, 56, 3.5, (254, 203, 197), 36)
+    PINEAPPLE  = (8, 62, 4.5, (242, 243, 23), 45)
+    MELON      = (9, 70, 5.0, (162, 239, 30), 55)
+    WATERMELON = (10, 80, 6.0, (33, 126, 22), 66)
 
-    def __init__(self, id: int, radius: int, mass: int, color: tuple[int, int, int]) -> None:
+    def __init__(self, id: int, radius: int, mass: int, color: tuple[int, int, int], score: int) -> None:
         self.id = id
         self.radius = radius
         self.mass = mass
         self.color = color
+        self.score = score
     
     @staticmethod
     def get_fruit_from_shape(shape: pymunk.Shape):
